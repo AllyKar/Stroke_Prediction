da400d9e947fc16a0c37bf90061ceb03bbd63fce
Alyssa <akarls2@wgu.edu>
2025-10-31 15:32:28 -0400

    created method to search for individual patient and print their information. created a method to sort and view high risk patients. cleaned up code, added comments

--- main.py
+++ main.py
@@ -12,18 +12,16 @@ model = joblib.load('rfr_cv.joblib')
 
 predictions = model.predict(df)
 
-print(predictions)
-
 results_df = pd.DataFrame({'Predicted': predictions})
 results_df.to_csv('random_forest_predictions.csv', index=False)
 
 df_with_predictions = df.copy()
-df_with_predictions[' Predicted Stroke Risk (%)'] = results_df
-
-print(df_with_predictions)
+df_with_predictions['Predicted Stroke Risk (%)'] = results_df
 
 df_with_predictions.to_csv('dataset_with_predictions.csv', index=False)
 
+print('Stroke Risk Predictions:')
+print(df_with_predictions)
 """
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
@@ -41,22 +39,16 @@ plt.show()
 df1 = pd.read_csv('dataset_with_predictions.csv')
 
 #Categorize High risk patients as patients with risk level higher than 75
-High_Risk = df1[df1[' Predicted Stroke Risk (%)'] > 75]
-print(High_Risk)
+High_Risk = df1[df1['Predicted Stroke Risk (%)'] > 75]
 HR_count = len(High_Risk.index)
-print(f"Number of High risk Patients: {HR_count}")
 
 #Categorize Medium risk patients as those with risk between 50 and 75
-Med_Risk = df1[(df1[' Predicted Stroke Risk (%)'] > 50) & (df1[' Predicted Stroke Risk (%)'] < 75)]
-print(Med_Risk)
+Med_Risk = df1[(df1['Predicted Stroke Risk (%)'] > 50) & (df1['Predicted Stroke Risk (%)'] < 75)]
 MR_count = len(Med_Risk.index)
-print(f"Number of Medium risk Patients: {MR_count}")
 
 #Categorize low risk patients as those with risk less than 50
-Low_Risk = df1[df1[' Predicted Stroke Risk (%)'] <50]
-print(Low_Risk)
+Low_Risk = df1[df1['Predicted Stroke Risk (%)'] <50]
 LR_count = len(Low_Risk.index)
-print(f"Number of Low risk Patients: {LR_count}")
 
 #Create Bar graph to show number of patients at each risk level
 import matplotlib.pyplot as plt
@@ -73,7 +65,7 @@ plt.ylabel('Number of Patients')
 plt.title('Stroke Risk')
 plt.show()
 
-plt.scatter(df2[' Predicted Stroke Risk (%)'], df2['Age'])
+plt.scatter(df2['Predicted Stroke Risk (%)'], df2['Age'])
 
 plt.xlabel('Stroke Risk')
 plt.ylabel('Age')
@@ -106,44 +98,43 @@ plt.show()
 #Create 10 patients for scatter plot
 pt1 = df1.iloc[1] #create data frame of a single patient
 pt1_count = (pt1 == 1).sum() #Find total number of Symptoms the patient has
-pt1_SR = pt1[' Predicted Stroke Risk (%)'] #find patients stroke risk prediction
+pt1_SR = pt1['Predicted Stroke Risk (%)'] #find patients stroke risk prediction
 
 pt2 = df1.iloc[2]
 pt2_count = (pt2 == 1).sum()
-pt2_SR = pt2[' Predicted Stroke Risk (%)']
-print(pt2_SR)
+pt2_SR = pt2['Predicted Stroke Risk (%)']
 
 pt3 = df1.iloc[3]
 pt3_count = (pt3 == 1).sum()
-pt3_SR = pt3[' Predicted Stroke Risk (%)']
+pt3_SR = pt3['Predicted Stroke Risk (%)']
 
 pt4 = df1.iloc[4]
 pt4_count = (pt4 == 1).sum()
-pt4_SR = pt4[' Predicted Stroke Risk (%)']
+pt4_SR = pt4['Predicted Stroke Risk (%)']
 
 pt5 = df1.iloc[5]
 pt5_count = (pt5 == 1).sum()
-pt5_SR = pt5[' Predicted Stroke Risk (%)']
+pt5_SR = pt5['Predicted Stroke Risk (%)']
 
 pt6 = df1.iloc[6]
 pt6_count = (pt6 == 1).sum()
-pt6_SR = pt6[' Predicted Stroke Risk (%)']
+pt6_SR = pt6['Predicted Stroke Risk (%)']
 
 pt7 = df1.iloc[7]
 pt7_count = (pt7 == 1).sum()
-pt7_SR = pt7[' Predicted Stroke Risk (%)']
+pt7_SR = pt7['Predicted Stroke Risk (%)']
 
 pt8 = df1.iloc[8]
 pt8_count = (pt8 == 1).sum()
-pt8_SR = pt8[' Predicted Stroke Risk (%)']
+pt8_SR = pt8['Predicted Stroke Risk (%)']
 
 pt9 = df1.iloc[9]
 pt9_count = (pt9 == 1).sum()
-pt9_SR = pt9[' Predicted Stroke Risk (%)']
+pt9_SR = pt9['Predicted Stroke Risk (%)']
 
 pt10 = df1.iloc[10]
 pt10_count = (pt10 == 1).sum()
-pt10_SR = pt10[' Predicted Stroke Risk (%)']
+pt10_SR = pt10['Predicted Stroke Risk (%)']
 
 #Create Scatter plot Stroke Risk vs. Number of symptoms
 x_vals = [pt1_SR, pt2_SR, pt3_SR, pt4_SR, pt5_SR, pt6_SR, pt7_SR, pt8_SR, pt9_SR, pt10_SR]
@@ -157,3 +148,12 @@ plt.title(f'Scatter Plot of Stroke Risk vs Symptoms')
 # Display the plot
 plt.show()
 
+n = 107
+print('\nPatient ', n , ':')
+print(df1.iloc[n])
+
+High_Risk_Sorted = High_Risk.sort_values(by='Predicted Stroke Risk (%)', ascending=False)
+print('\nHigh Risk Patients')
+print('Number of High Risk Patients:', HR_count)
+
+print(High_Risk_Sorted.head(25))
--- rfr.py
+++ rfr.py
@@ -12,7 +12,10 @@ print(df.head())
 #drop binary column
 df = df.drop(columns = ['At Risk (Binary)'])
 
+"""
+#view df to test modifications
 print(df.head())
+"""
 
 X = df.drop(['Stroke Risk (%)'], axis = 1)
 y = df['Stroke Risk (%)']
@@ -24,18 +27,18 @@ from sklearn.ensemble import RandomForestRegressor
 rfr = RandomForestRegressor(random_state = 15)
 rfr.fit(X_train, y_train)
 
+"""
 # To view the first few rows of the training features
-
 print(X_test.head())
 
 # To view the first few rows of the testing targets
-
 print(y_test.head())
+"""
 
-
+#run rfr on test data
 y_pred = rfr.predict(X_test)
 
-
+#test accuracy of model using mean absolute avg, mean squared error, and r2 score
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 mean_absolute_error(y_pred,y_test)
 mean_squared_error(y_pred,y_test)
@@ -46,7 +49,7 @@ print(mean_squared_error(y_pred,y_test))
 print(r2_score(y_pred,y_test))
 
 
-
+#set parameters to train rfr
 param_grid = {
     'n_estimators': [150, 175, 200],
     'max_depth': [10,20,30],
@@ -54,19 +57,24 @@ param_grid = {
     'min_samples_leaf': [1,5,10],
 }
 
+#create new rfr with parameters
 from sklearn.model_selection import GridSearchCV
 rfr_cv = GridSearchCV(estimator=rfr, param_grid=param_grid, cv=3, scoring='neg_mean_squared_error', n_jobs=-1)
 rfr_cv.fit(X_train, y_train)
 
+#run new model on test data
 y_pred = rfr_cv.predict(X_test)
 
+#test accuracy of new model using mean absolute avg, mean squared error, and r2 score
 mean_absolute_error(y_pred,y_test)
 mean_squared_error(y_pred,y_test)
 r2_score(y_pred, y_test)
+
 print(mean_absolute_error(y_pred,y_test))
 print(mean_squared_error(y_pred,y_test))
 print(r2_score(y_pred,y_test))
 
+#save model
 import joblib
 joblib.dump(rfr_cv,'rfr_cv.joblib')
 
